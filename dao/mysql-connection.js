1a24a58f4ae086f07a3147e50ea8420addf3b248
Behnam Hajian <behnam@ca.ibm.com>
2016-08-17 23:41:47 -0400

    DB pooling

---
+++ dao/mysql-connection.js
@@ -0,0 +1,16 @@
+/**
+ * Created by behnamhajian on 2016-08-17.
+ */
+var mysql = require('mysql');
+
+var pool  = mysql.createPool({
+  connectionLimit : 10,
+  host            : 'localhost',
+  user            : 'root',
+  password        : 'root',
+  database        : 'DB'
+});
+
+pool.getConnection(function(err, connection) {
+  // connected! (unless `err` is set)
+});
